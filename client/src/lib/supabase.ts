02f761c7ec390a5d04b09f206ad3b7279bb00b6a
linuxbyter <makau1.peter@gmail.com>
2026-02-13 05:13:28 +0300

    Update supabase.ts

--- client/src/lib/supabase.ts
+++ client/src/lib/supabase.ts
@@ -1,8 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = https://ioozkbjskhxphedlwuwl.supabase.co
-const supabaseAnonKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlvb3prYmpza2h4cGhlZGx3dXdsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc4MDg5NzksImV4cCI6MjA4MzM4NDk3OX0.K5xrBqhL5gjpB-_A1HQWTgCI5xeLxBM0hj4si477dJQ
-
+const supabaseUrl = "https://ioozkbjskhxphedlwuwl.supabase.co"
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlvb3prYmpza2h4cGhlZGx3dXdsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc4MDg5NzksImV4cCI6MjA4MzM4NDk3OX0.K5xrBqhL5gjpB-_A1HQWTgCI5xeLxBM0hj4si477dJQ"
 console.log('Supabase Init Check:', { 
   hasUrl: !!supabaseUrl, 
   hasKey: !!supabaseAnonKey,
